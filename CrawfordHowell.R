ca46ed25ed34e50ba046cb281aaf2190e9bc1de1
Chi-Hsun (Eric) Chang <40210187+Eric-CH-Chang@users.noreply.github.com>
2019-02-12 17:05:28 -0500

    Update CrawfordHowell.R

--- CrawfordHowell.R
+++ CrawfordHowell.R
@@ -1,4 +1,8 @@
 # Crawford-Howell t-test for single case studies
+#' 
+#' @param case Data of a single case subject 
+#' @param control Data of a group of subjects for comparison 
+#' 
 CrawfordHowell <- function(case, control){
   tval <- (case - mean(control)) / (sd(control)*sqrt((length(control)+1) / length(control)))
   degfree <- length(control)-1
